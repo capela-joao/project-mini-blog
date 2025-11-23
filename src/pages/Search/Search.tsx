b0be9ce8270edf1405252355e57fdbd27db6ca3a
capela-joao <joaopaulo1060@hotmail.com>
2025-11-22 23:21:43 -0300

    Estilização page Search

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,12 +1,34 @@
 import styles from './Search.module.css';
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 import { useQuery } from '../../hooks/useQuery';
+import PostDetail from '../../components/PostDetail/PostDetail';
+import { Link } from 'react-router-dom';
+
 const Search = () => {
   const query = useQuery();
   const search = query.get('q');
+
+  const { documents: posts } = useFetchDocuments('posts');
+
   return (
-    <div>
+    <div className={styles.search}>
       <h2>Search</h2>
+      <div>
+        {posts && posts.length === 0 && (
+          <>
+            <p>
+              Não foram encontrados posts na sua busca...
+            </p>
+            <Link to="/" className="btn">
+              Voltar
+            </Link>
+          </>
+        )}
+        {posts &&
+          posts.map((post) => (
+            <PostDetail key={post.id} post={post} />
+          ))}
+      </div>
       <p>{search}</p>
     </div>
   );
